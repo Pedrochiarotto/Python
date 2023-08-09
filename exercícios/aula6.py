d1cdac7ced03856d9f93bbb76278066d736c2735
Pedrochiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2023-08-09 18:02:11 -0300

    Update aula6.py

--- exercícios/aula6.py
+++ exercícios/aula6.py
@@ -1,5 +1,12 @@
 
+"""
+conversão de tipos de dados
+Tipos: str,int,float,bool
+
+
+"""
  
+
 
 
 print(1 + 1)
@@ -7,4 +14,5 @@ print('a' + 'b')
 #o Python não concatena 2 tipos diferentes.
 
 #convertendo dados
+print(type(int("1") + 1))
 
