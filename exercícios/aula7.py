d05ccd69767f9d5319bc86f5d65e971929c78072
Pedrochiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2023-08-10 09:29:02 -0300

    Variaveis

---
+++ exercícios/aula7.py
@@ -0,0 +1,15 @@
+#Variáveis
+#A PEP8 nos permite criar váriaveis com letras minúsculas, pode usar numeros(Depois da letra) e underline"_"
+#Para atribuir uma variável, usamos o símbolo =
+
+nomeCompleto = "Pedro Henrique Chiarotto"
+soma = 2 + 2
+print(nomeCompleto, soma)
+
+nome = "Pedro"
+idade = 18
+maiorIdade = idade >= 18 
+
+print("Nome:",nome, "Idade:", idade, "É maior de idade?:", maiorIdade)
+
+
---
+++ exercícios/aula8.py
@@ -0,0 +1,17 @@
+#Exercício variáveis
+
+
+nome = "Pedro"
+sobrenome = "Chiarotto"
+idade = 18
+anoNascimento = 2004 
+maiorDeIdade = idade >= 18
+alturaEmMetros = 1.84
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print("Idade:", idade)
+print('Ano de nascimento:', anoNascimento)
+print('Maior de idade?:', maiorDeIdade)
+print('Altura em metros:', alturaEmMetros)
