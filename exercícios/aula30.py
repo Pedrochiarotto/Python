3fac3212d66bab718df6ae9c4ccfce9c98d0bc1c
Pedrochiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2023-08-14 19:46:19 -0300

    ;

--- exercícios/aula30.py
+++ exercícios/aula30.py
@@ -4,4 +4,16 @@ None = Não valor
 is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
+condicao = False
+passouIf = None
+
+if condicao:
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+if passouIf is None:
+    print("Não passou no if")
+else:                           #is not none
+    print("Passou no if")
 
