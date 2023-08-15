79bd6f11dd7b56604b17c82e43a46123d7b7d914
Pedrochiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2023-08-15 15:08:41 -0300

    Create aula32.py

---
+++ exercícios/aula32.py
@@ -0,0 +1,7 @@
+""" 
+
+https://docs.python.org/pt-br/3/library/stdtypes.html
+ja vimos os tipos IMUTAVEIS(Não pode ser mudado no decorrer do código) : str(string), int(inteiro), float(real; ponto flutuante), bool(True or False)
+"""
+
+string = "Pedro"
