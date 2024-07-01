8d87e271ba22ff3212df438c00158f1aec141a5f
Pedro Henrique Chiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2024-07-01 19:08:16 -0300

    Update aula46.py

--- exercícios/aula46.py
+++ exercícios/aula46.py
@@ -1,5 +1,21 @@
 #Listas em python!!
+#Tipo list - mutável
+#Suporta diversos valores de qualquer tipo 
 
-string = 'abcde' #5 caracter
+"""
+Métodos: append, insert, pop, del, clear, extend, +
+"""
+
+string = 'abcde' #5 caracter (len)
 # lista = list()
-lista = []
+lista = [123,True, 'Luiz', 4.56]
+print(lista)
+# print(bool(lista))
+# print (lista, type(lista))
+
+#Consultar um elemento da lista\/, por exemplo o elemento 2 entre []
+print (lista[2], type(lista[2]))
+
+#Alterar valor de um elemento na lista
+lista[2] = 'Pedro'
+print(lista[2])
