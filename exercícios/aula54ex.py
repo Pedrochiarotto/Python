bf775fd415525b3199631e248b3d36547dd7e7f9
Pedro Henrique Chiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2024-07-16 18:09:29 -0300

    Update aula54ex.py

--- exercícios/aula54ex.py
+++ exercícios/aula54ex.py
@@ -3,12 +3,24 @@
 
 lista = []
 continuar = True 
-"""resposta = input("Deseja continuar manipulando a lista? [s/n]") 
-if resposta == 's':
-    continuar = False"""
+while(continuar == True):
+    r = str(input("Selecione uma opção: \n [i]-Inserir [a]-Apagar [l]-Listar [f]-Finalizar"))
+    if r == 'i': 
+        valor = input("Valor: ")
+        lista.append(valor)
+    elif r == 'a':
+        valor = input("Qual valor deseja apagar?") 
+        if valor in lista:
+            lista.remove(valor)
+        else:
+            print("Insira um valor valido")
+    elif r == 'l':
+        if range(len(lista)) != 0:
+            print(lista)
+        else: 
+            print("Lista vazia")
+    elif r == 'f':
+        continuar == False
+    
 
-
     
-    
-    
-     
