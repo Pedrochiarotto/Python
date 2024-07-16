bf9e5d5bd0c5d743d771ff1db146c207b0d12c2b
Pedro Henrique Chiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2024-07-16 18:38:38 -0300

    Fim do exercicio

--- exercícios/aula54ex.py
+++ exercícios/aula54ex.py
@@ -7,18 +7,21 @@ while(True):
     if r == 'i': 
         valor = input("Valor: ")
         lista.append(valor)
-    elif r == 'a':
-        valor = input("Qual valor deseja apagar?") 
-        if valor in lista:
-            lista.remove(valor)
-        else:
-            print("Insira um valor existente")
     elif r == 'l':
         if len(lista) == 0:
             print("Lista vazia")
         else: 
             for indice, valor in enumerate(lista):
                 print(indice,valor)
+    elif r == 'a':
+        indice_str = input("Qual Item da lista deseja apagar?") 
+        try: 
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:#O que ira printar na tela caso o valor digitado nao seja um numero inteiro 
+            print("Digite um número inteiro")
+        except IndexError: #O que ira printar na tela caso o número digitado nao esteja na lista
+            print("Indice nao existe na lista")
     else:
         print("Escolha alguma das opções (i, a ou l)")
     
